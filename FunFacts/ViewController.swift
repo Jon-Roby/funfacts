095964fe746815ab844a756ed9ac3c7bc60bd8eb
Jon Roby <jonathan.roby1@gmail.com>
2015-07-23 09:04:10 -0700

    Add label and button

--- FunFacts/ViewController.swift
+++ FunFacts/ViewController.swift
@@ -9,10 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var funFactLabel: UILabel!
+    
+    let factsArray = ["Ants stretch when they wake up in the morning.", "Ostritches can run faster than horses"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        funFactLabel.text = factsArray[0]
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +25,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func showFunFact() {
+        
+        funFactLabel.text = factsArray[1]
+    }
 
 }
 
